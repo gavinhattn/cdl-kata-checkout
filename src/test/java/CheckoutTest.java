8b693591ba14e8f510f4e93109c4c8c12c25bde5
gavinhatton <gavinhatton90@googlemail.com>
2024-10-03 15:06:39 +0100

    Initial setup of project and adding first checkout unit test (red phase)

---
+++ src/test/java/CheckoutTest.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CheckoutTest {
+
+    @Test
+    public void testScanSingleItemA() {
+        //given
+        Checkout checkout = new Checkout();
+
+        //when
+        checkout.scan("A");
+        int total = checkout.getTotal();
+
+        //then
+        Assert.assertEquals(50, total);
+    }
+}
