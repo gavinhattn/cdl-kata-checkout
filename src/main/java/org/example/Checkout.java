94fb3c35ba05529afe76c5bf1a65aff355c9cb4d
gavinhatton <gavinhatton90@googlemail.com>
2024-10-03 15:51:35 +0100

    add code to calculate special offers to pass special offer test (green phase)

--- src/main/java/org/example/Checkout.java
+++ src/main/java/org/example/Checkout.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 public class Checkout {
     private Map<String, Integer> prices = new HashMap<>();
+    private Map<String, Integer> specialPrices = new HashMap<>();
+    private Map<String, Integer> itemCounts = new HashMap<>();
     private int total = 0;
 
     public Checkout() {
@@ -13,10 +15,24 @@ public class Checkout {
         prices.put("B", 30);
         prices.put("C", 20);
         prices.put("D", 15);
+
+        //hardcoded values for green phase of TDD, to test special price requirement
+        specialPrices.put("A", 130);
     }
 
     public void scan (String item){
         if (prices.containsKey(item)) {
+            itemCounts.put(item, itemCounts.getOrDefault(item, 0) + 1);
+            applyPrice(item);
+        }
+    }
+
+    public void applyPrice(String item){
+        int count = itemCounts.get(item);
+        if (item.equals("A") && count % 3 == 0) {
+            total -= 2 * prices.get(item);
+            total += prices.get(item);
+        } else {
             total += prices.get(item);
         }
     }
