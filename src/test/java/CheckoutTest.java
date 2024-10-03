ec5ce741caa45bf3a5924ba3d612d55d05949463
gavinhatton <gavinhatton90@googlemail.com>
2024-10-03 16:49:33 +0100

    add test for adding itemPrices and specialPrices

--- src/test/java/CheckoutTest.java
+++ src/test/java/CheckoutTest.java
@@ -50,17 +50,19 @@ public class CheckoutTest {
     public void testScanUnknownItem() {
         //given
         Checkout checkout = new Checkout();
+
+        // make console output visible
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
         //when
-        checkout.scan("E");
+        checkout.scan("UNK");
         int total = checkout.getTotal();
 
         //then
         Assert.assertEquals(0, total);
         String output = outContent.toString().trim();
-        Assert.assertEquals("Item E is not recognised.", output);
+        Assert.assertEquals("Item UNK is not recognised.", output);
     }
 
     @Test
@@ -107,6 +109,38 @@ public class CheckoutTest {
 
         //then
         Assert.assertEquals(45, total);  // 2 B's for 45 pence
+    }
+
+    @Test
+    public void testAddItemPrice() {
+
+        //given
+        Checkout checkout = new Checkout();
+        checkout.addItemPrice("E", 25); // Add a new item with a price of 25 pence
+
+        //when
+        checkout.scan("E");
+        int total = checkout.getTotal();
+
+        //then
+        Assert.assertEquals(25, total);
+    }
+
+    @Test
+    public void testAddSpecialPrice() {
+
+        //given
+        Checkout checkout = new Checkout();
+        checkout.addItemPrice("F", 40); // Add a price for item F
+        checkout.addSpecialPrice("F", 2, 70); // Add a special price for 2 F's
+
+        //when
+        checkout.scan("F");
+        checkout.scan("F");
+        int total = checkout.getTotal();
+
+        //then
+        Assert.assertEquals(70, total);
     }
 
 
